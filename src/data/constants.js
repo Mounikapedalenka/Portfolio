7997b8d8d851cdf3366c470210c59b458c755001
MounikaPedalenka <mounikapedalenka273@gmail.com>
2024-11-24 14:13:13 +0530

    intial

---
+++ src/data/constants.js
@@ -0,0 +1,334 @@
+export const Bio = {
+  name: "Mounika Pedalenka",
+  roles: [
+    "Full Stack Developer",
+    "Web Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Mounikapedalenka",
+  hackerank:"https://www.hackerrank.com/profile/21341A05D5",
+  codechef:"https://www.codechef.com/users/p_mounika123",
+  resume:
+    "https://drive.google.com/file/d/1O7F9DIyrt7-S8sdsdWN84TPAEwCiWSvP/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/mounika-pedalenka-a95081247/",
+  insta: "https://www.instagram.com/mounika_7_6_0_3/?__pwa=1",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Ms Office",
+    skills: [
+      {
+        name: "Ms Word",
+        image:
+          "https://th.bing.com/th/id/OIP.34gJAmet2M-xK7NQNnMzsQHaG4?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Ms PPT",
+        image:
+          "https://logodownload.org/wp-content/uploads/2020/04/microsoft-powerpoint-logo.png",
+      },
+      {
+        name: "MS Excel",
+        image:
+          "https://th.bing.com/th/id/OIP.6INpILSfB6MhnEYHlvEN4QHaG4?rs=1&pid=ImgDetMain",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://th.bing.com/th/id/OIP.SBWyzFav5bWzgLC8uH4M8QHaHa?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Springboot",
+        image:
+          "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjt-h5BnuWvoOMEVwD386k6fd-reYDF0VfY-e4SGoKXMb3mGcgEDtejEnmQDbCIHv09BeozLJEqU7y8iGktyob_4tnPQKlJXUT97qmVD7xQRrzoDKcYQTtq-mtgNjDJ_IB0kScGfndVdaD1dDivojIJ3Eia2hS-3Z4UoiV9o7VIFoavplpyfkWRGNnSlds/w447-h447/spring-boot-1_5zDxm9B.webp",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "CockroachDB",
+        image: "https://th.bing.com/th/id/OIP.r7qo2LSbfpVcB8bYjxaDDwAAAA?w=474&h=476&rs=1&pid=ImgDetMain",
+      },
+    ],
+  },
+
+  {
+    title: "Tools And Utilities",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://th.bing.com/th/id/OIP.Xa0BEkwl0Zx4qnY9lMbD7gHaHa?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Eclipse IDE",
+        image:
+          "https://www.wizcase.com/wp-content/uploads/2021/05/Eclipse-IDE-Logo.jpg",
+      },
+      {
+        name: "DBeaver",
+        image:
+          "https://th.bing.com/th/id/OIP.1fcoLbMuLDLREk7JKJqF-QAAAA?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://th.bing.com/th/id/OIP.bRJkrf41UWUHit6HPYir5QHaHa?w=2160&h=2160&rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://th.bing.com/th/id/OIP.-IIWW4BAttaJhPz7PNRyygHaHa?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Basics of Camunda",
+        image:
+          "https://camunda.com/wp-content/uploads/2020/07/camunda-logo-social-update.jpg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/3c1cb0128162891.6150eba27c6d4.png",
+    company: "Verzeo",
+    date: "May 2022 - June 2022",
+    desc: "Worked on Vezeo Platforms developing machine learning models using Python to automate decision-making processes.",
+    skills: [
+      "Python",
+      "NumPy",
+      "Pandas",
+      "Matplotlib",
+      "Seaborn",
+      "Scikit-learn",
+      "TensorFlow",
+      "Data Preprocessing",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://images.crunchbase.com/image/upload/c_pad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1509740922/eqshonkzsrszm4djrram.png",
+    role: "React Js Intern",
+    company: "Rise Pvt Ltd",
+    date: "June 2023 - Aug 2023",
+    desc: "Working on the  web application using HTML,CSS,Javascript,React js",
+    skills: [
+      "ReactJS",
+      "NodeJs",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://th.bing.com/th/id/OIP.hLCAKgDX6_K1DysMaTWQBAHaHa?rs=1&pid=ImgDetMain",
+    role: "Java Internship",
+    company: "Internship Studio",
+    date: "July 2024 - August 2024",
+    desc: "Built an employee management full-stack web application using Java. Recognized as the top performer in the program.",
+skills: [
+  "Java",
+  "Spring Boot",
+  "REST APIs",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "MySQL"
+],
+  },
+  {
+    id: 2,
+    img: "https://adtechtoday.com/wp-content/uploads/2024/06/Kshema-768x459.webp",
+    role: "Backend Developer",
+    company: "Kshema General Insurance Pvt Ltd",
+    date: "August 2024 - November 2024",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    skills: [
+      "Java",
+      "Spring Boot",
+      "REST APIs",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MySQL",
+      "Camunda",
+      "CockroachDB",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://th.bing.com/th/id/OIP.9R2PcKNOQqc0DTWRyOASdwAAAA?rs=1&pid=ImgDetMain",
+    school: "GMR Institute Of Technology, Razam",
+    date: "Oct 2021 - May 2025",
+    grade: "9.18 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at GMR Institute Of Technology, Razam. I have completed 6 semesters and have a CGPA of 9.18. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the STEM Club , where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://th.bing.com/th/id/R.5f8832adbe22b2c21086c4891155e58e?rik=bt9%2fVQtlKU3aYw&riu=http%3a%2f%2fnetxplore.in%2fassets%2fimg%2fcollege_logo%2fsri_aditya_junior_college_rajam_andhra_pradesh.jpg&ehk=N0s3Na9CJongfyBqA8biPE78xNirOzKzeXKXtOS2LvQ%3d&risl=&pid=ImgRaw&r=0",
+    school: "Sri Aditya Junior College, Razam",
+    date: "Apr 2019 - Apr 2021`",
+    grade: "95.2%",
+    desc: "I completed my class 12 high school education at Sri Aditya Junior College, Razam, where I studied MPC.",
+    degree: "MPC",
+  },
+  {
+    id: 2,
+    img: "https://is4-ssl.mzstatic.com/image/thumb/Purple115/v4/07/4e/7e/074e7e57-5901-2637-0908-5757056bc8fd/source/512x512bb.jpg",
+    school: "Viswsanthi Vidyalayam, Razam",
+    date: "Apr 2017 - Apr 2019",
+    grade: "93.1%",
+    desc: "I completed my class 10 education at Viswsanthi Vidyalayam, Razam.",
+    degree: "SSC",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Web Application Using Mern Stack",
+    date: "July 2023 - August 2023",
+    description:
+      "Introducing a web application built using the MERN stack (React.js, and Node.js), designed to streamline task management and enhance workplace productivity.It provides a user-friendly interface,Employee Portal,Admin Dashboard",
+    image:
+      "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/248656564/original/283f3519803935de3613595ee7c4a68e7b7ae133/create-a-web-application.png",
+    tags: [
+      "Xampp",
+      "React Js",
+      "Eclipse IDE",
+      "VsCode",
+      "Node Js",
+    ],
+    category: "web app",
+    github: "https://github.com/Mounikapedalenka/React-Js",
+  },
+  {
+    id: 0,
+    title: "Online bidding",
+    date: "Apr 2024 - May 2024",
+    description:
+      "Developed a full-stack web application for online bidding using JavaScript (React JS for the frontend and Node.js for the backend), we can break down the solution into different components. Here’s an overview of how this could be structured",
+    image:
+      "https://cdn.dribbble.com/users/7258245/screenshots/15489111/8_4x.png",
+    tags: ["HTML", "Node Js", "CSS ", "JavaScript","Vscode","Eclipse IDE"],
+    category: "web app",
+    github: "https://github.com/Mounikapedalenka/Online-Bidding-Web",
+  },
+  {
+    id: 1,
+    title: "Restaurant Billing system",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "The Restaurant Menu and Billing System is a Java-based application designed to allow users to interact with the restaurant's menu, select items, and generate a bill using Java Applets. This system aims to automate the ordering process, improve customer service, and provide a seamless billing experience for both customers and restaurant staff.",
+    image:
+      "https://i.ytimg.com/vi/BSp1I4sDPQ8/maxresdefault.jpg",
+    tags: [
+      "Java",
+      "Java applets",
+    ],
+    category: "android app",
+    github: "https://github.com/Mounikapedalenka/Java-Projects/blob/main/Check.java",
+  },
+  {
+    id: 2,
+    title: "Pizza Billing Sytem",
+    date: "Jan 2024",
+    description:
+      "The Pizza Billing System is a Java-based desktop application designed to facilitate the ordering and billing process for a pizza restaurant. It allows customers to place orders, choose different pizza types, customize toppings, and generate a bill based on their selections.",
+    image:
+      "https://media.geeksforgeeks.org/wp-content/uploads/20200701231815/p7-660x401.PNG",
+    tags: ["Java",
+      "Java applets",
+    ],
+    category: "android app",
+    github: "https://github.com/Mounikapedalenka/Java-Projects/blob/main/Pizza.java",
+  },
+  {
+    id: 3,
+    title: "Working on machine learning models",
+    date: "october 2022 - November 2022",
+    description:
+      "Regression Analysis is a statistical method used to examine the relationship between one or more independent variables (predictors) and a dependent variable (outcome). The goal of regression analysis is to model this relationship so that the dependent variable can be predicted based on the values of the independent variables.",
+    image:
+      "https://th.bing.com/th/id/OIP.JWBeTBtvkVD-ezqjG04ieQHaFs?rs=1&pid=ImgDetMain",
+    tags: ["python","Machine Learning","keras","sklearn","Matplotlib"],
+    category: "machine learning",
+    github: "https://github.com/Mounikapedalenka/Machine-Learning",
+  },
+  {
+    id: 10,
+    title: "Runway Lighting System",
+    date: "February 2024 - May 2024",
+    description:
+      "The Runway Lighting System in Tinkercad simulates the lighting used on airport runways to guide aircraft during takeoff and landing, especially in low visibility conditions. It typically includes edge lights, threshold lights, and centerline lights, arranged along the runway's length.",
+    image:
+      "https://csg.tinkercad.com/things/7cH3eVSSXLb/t725.png?rev=1664283744384000000&s=&v=1&type=circuits",
+    tags: ["C Language","Tinker Cad"],
+    category: "IOT",
+    github: "https://github.com/Mounikapedalenka/IOT",
+  },
+];
